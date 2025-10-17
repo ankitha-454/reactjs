1063c9cd62b552b2d4ef7b60f7ef5fad4a0b396d
Ankitha <ankithapoojary454@gmail.com>
2025-10-17 10:56:54 +0530

    third commit

--- task1/src/Demo2.js
+++ task1/src/Demo2.js
@@ -1,22 +1,57 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import "./App.css";
+import style from "./Button.module.css";
 
 function Demo() {
+  //hooks
+  const [count, setCount] = useState(0);
+  const handleCountChange = () => {
+    setCount(count + 1);
+  };
+  const handleCountChange1 = () => {
+    setCount(count - 1);
+  };
+
+  // const [color, setColor] = useState("red");
+  // const handleColorChange = () => {
+  //   setColor("blue");
+  // };
+  //   const myStyle = {
+  //     color: "white",
+  //     backgroundColor: "red",
+  //     padding: "10px",
+  //   };
+
   return (
-    <div>
-      Choose one
-      <div>
-        <li>
-          <Link to="/home">Home</Link>
-        </li>
-        <li>
-          <Link to="/blogs">Blogs</Link>
-        </li>
-        <li>
-          <Link to="/contact">Contact</Link>
-        </li>
-      </div>
+    <div className="demo">
+      <div>{count}</div>
+      <button onClick={handleCountChange}>Click to increment</button>
+      <button onClick={handleCountChange1}>Click to decrement</button>
     </div>
+    // <div className="demo">
+    //   <div>{color}</div>
+    //   <button onClick={handleColorChange}>Click to change</button>
+    // </div>
+    //     <>
+    //       <h1 style={myStyle}>Hello style</h1>
+    //       <p>Add a little style</p>
+    //     </>
+    //     <div className="demo">
+    //       Choose one
+    //       <div >
+    //         <li>
+    //           <Link to="/home">Home</Link>
+    //         </li>
+    //         <li>
+    //           <Link to="/blogs">Blogs</Link>
+    //         </li>
+    //         <li>
+    //           <Link to="/contact">Contact</Link>
+    //         </li>
+    //       </div>
+    //       <button className={style.button}>Submit</button>
+    //     </div>
   );
 }
 
--- task1/src/Home.js
+++ task1/src/Home.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Home() {
   return (
-    <div>Welcome to MyHome</div>
+    <div style={{color:"red"}}>Welcome to MyHome</div>
   )
 }
 
